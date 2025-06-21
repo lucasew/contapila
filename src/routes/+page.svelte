6e76fbbfff1106d59fb5c2aebad81c015db3c211
lucasew <lucas59356@gmail.com>
2025-06-21 16:33:42 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,35 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import {
+		createCoreBeancountModule,
+		createCustomReportingModule,
+		createParser,
+		createTransactionModule
+	} from '$lib/beancount.js';
+
+	let files = $state();
+	let content = $state([]);
+	$effect(() => {
+		console.log(files);
+		if (!files) return;
+		if (files.length != 1) return;
+		files
+			.item('utf-8')
+			.text()
+			.then((text) => (content = parser(text)));
+	});
+	const parser = createParser({
+		modules: [
+			createCoreBeancountModule(),
+			createTransactionModule(),
+			createCustomReportingModule()
+		],
+		fieldParsers: {},
+		customValidators: {}
+	});
+</script>
+
+<h1>Beancount preview</h1>
+
+<input type="file" bind:files />
+
+<pre>{content}</pre>
