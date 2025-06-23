3667d0fbf1d86a2a413d3485f169b04882c5bb28
lucasew <lucas59356@gmail.com>
2025-06-23 15:25:56 -0300

    refactor(ui): encapsula seletor de tema em componente ThemeToggle usando Sveltestrap ThemeToggler

---
+++ src/lib/ThemeToggle.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { Button, Icon, ThemeToggler } from '@sveltestrap/sveltestrap';
+
+
+</script>
+
+<ThemeToggler let:currentColorMode let:toggleColorMode>
+<Button outline aria-label="Alternar tema" on:click={() => toggleColorMode()}>
+    <Icon name={currentColorMode === 'light' ? 'moon' : 'sun'} />
+</Button> 
+</ThemeToggler>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { Navbar, NavbarBrand, Container } from '@sveltestrap/sveltestrap';
+import ThemeToggle from '$lib/ThemeToggle.svelte';
+
+</script>
+
+<Navbar color="dark" dark class="mb-4">
+  <Container class="d-flex justify-content-between align-items-center">
+    <NavbarBrand href="/">Contapila</NavbarBrand>
+    <ThemeToggle />
+  </Container>
+</Navbar>
+<Container>
+  <slot />
+</Container>
