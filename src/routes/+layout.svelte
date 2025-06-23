595199eb4551a7cb9e7f21d3a97c361c8ba5f8e8
lucasew <lucas59356@gmail.com>
2025-06-23 17:03:34 -0300

    style: ajustes e melhorias visuais no layout principal (não relacionados à tabela/accordion)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-import { Navbar, NavbarBrand, Container } from '@sveltestrap/sveltestrap';
+import { Navbar, NavbarBrand, Container, Styles } from '@sveltestrap/sveltestrap';
 import ThemeToggle from '$lib/ThemeToggle.svelte';
 
 </script>
-
-<Navbar color="dark" dark class="mb-4">
-  <Container class="d-flex justify-content-between align-items-center">
+<Styles/>
+<Navbar>
     <NavbarBrand href="/">Contapila</NavbarBrand>
     <ThemeToggle />
-  </Container>
 </Navbar>
 <Container>
   <slot />
